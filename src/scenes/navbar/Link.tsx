a2a8aa35208e2fa3d6ae60fe7850048469f12494
hiennd482 <fayolo482@gmail.com>
2023-07-05 16:12:43 +0700

    third-commit

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+// import React from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import Types, { SelectedPage } from "@/Types/types";
-
-const Link = ({ page, selectedPage, setSelectedPage }: Types) => {
+import { SelectedPage, Props } from "@/Types/types";
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
   const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
 
   return (
